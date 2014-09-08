7d0642f41c2f3e9127fbf1c8e59e9919cce084c5
Corey <burkleycorey@yahoo.com>
2014-09-08 11:36:22 -0500

    created new rails application
    Removed username and password requirements in the database.yml file to use a socket connection to connect to the database.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPortfolio::Application.config.secret_token = 'f541bfd8c515b5ad6845f0b51ee928b99284cc59e43b5c2a93ebd72e1adce707c51d96c99f444e72f8197cbe934b9aee05a7cca75dd018ac32971f29e3f45a21'
