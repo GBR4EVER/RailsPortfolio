7a3ef7e7dfebc98fbff2def5927bd907c4706229
Corey <burkleycorey@yahoo.com>
2014-09-10 09:32:35 -0500

    Had to reorder my show route to bottom to fix the new route.  In the future use 'resources' directive in routes to perform CRUD operations.

--- config/routes.rb
+++ config/routes.rb
@@ -10,24 +10,24 @@ RailsPortfolio::Application.routes.draw do
   
   get "classprojects" => 'class_projects#index', :as => "class_project"
   
-  get "classprojects/:id" => 'class_projects#show', :as => "project"
-  
   get "classprojects/new" => 'class_projects#new', :as => "project_new"
   get "classprojects/:id/edit" => 'class_projects#edit', :as => "project_edit"
   
   post "classprojects" => 'class_projects#create' # Notice that the path 'classprojects' is the same as the index path.
   put "classprojects/:id" => 'class_projects#update' # Notice that the path 'classprojects/:id' is the same as the show route.
   
+  get "classprojects/:id" => 'class_projects#show', :as => "project"
+   
   #--------------Contacts Model----------------------#
   
   get "contacts" => 'contacts#index', :as => "contact_list"
-  
-  get "contacts/:id" => 'contacts#show', :as => "contact"
   
   get "contacts/new" => 'contacts#new', :as => "contact_new"
   get "contacts/:id/edit" => 'contact#edit', :as => "contact_edit"
   
   post "contacts" => 'contacts#create'
   put "contacts/:id" => 'contacts#update'
+  
+  get "contacts/:id" => 'contacts#show', :as => "contact"
   
 end
