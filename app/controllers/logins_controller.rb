a17817446e13bb1612c8c75ebedc807026b702a2
Corey <burkleycorey@yahoo.com>
2014-09-12 17:26:18 -0500

    Fixed login authentication

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,11 +4,12 @@ class LoginsController < ApplicationController
   end
  
   def create
+    # Check if the email/password is a match to log someone in.
     user = User.find_by_email(params[:email])
     
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to article_list_path
+      redirect_to welcome_path
     else
       raise "Invalid login."
     end
